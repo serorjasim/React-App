719fd48ac0348974e0444ee2c3065caff4faee4c
seror <seror94ab@yahoo.com>
2018-08-13 17:22:03 -0500

    created a property for user input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,8 +8,11 @@ class StorePicker extends React.Component {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Get the text from that input
+    const storeName = this.myInput.current.value;
     // 3. Change the page to /store/whatever-they-entered
-  }
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
