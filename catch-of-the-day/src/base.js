d6cb00324da876790dca3f9a95765d8aa78100f5
seror <seror94ab@yahoo.com>
2018-08-15 15:11:59 -0500

    exporting base and firebase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -5,4 +5,12 @@ const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDp0ZX1WPBe5c33HvXjvGzN1w67K0RyADU",
   authDomain: "catch-of-the-day-seror.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-seror.firebaseio.com"
-})
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// This is a named export
+export { firebaseApp };
+
+// This is a default export
+export default base;
