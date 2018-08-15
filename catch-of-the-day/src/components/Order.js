3726247d3b56ed06a5c6f2fd40ea98574b6607e2
seror <seror94ab@yahoo.com>
2018-08-15 12:55:47 -0500

    displying amount charged in order component

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 class Order extends React.Component {
-    render() {
-        return <div className="order">Order!!!</div>;
-    }
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        {orderIds}
+        <div className="total">
+          <strong>{total}</strong>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Order;
