5a48a4148c30db4e525c238b9477ec6cd54e9504
seror <seror94ab@yahoo.com>
2018-08-15 17:03:47 -0500

    displayed and mounted the fishes and the order

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDp0ZX1WPBe5c33HvXjvGzN1w67K0RyADU",
-  authDomain: "catch-of-the-day-seror.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-seror.firebaseio.com"
+  apiKey: "AIzaSyCR3hJPcjGrvT8fmybQlRIoEnsH_jb_MQ8",
+  authDomain: "catch-of-the-day-seror-cab44.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-seror-cab44.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
