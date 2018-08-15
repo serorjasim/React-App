d08d8a8dde56e959e3030282dd1fdd39e5423853
seror <seror94ab@yahoo.com>
2018-08-15 15:07:15 -0500

    grabbed the info to connect to firebase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,2 +1,8 @@
 import Rebase from "re-base";
 import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDp0ZX1WPBe5c33HvXjvGzN1w67K0RyADU",
+  authDomain: "catch-of-the-day-seror.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-seror.firebaseio.com"
+})
