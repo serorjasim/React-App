7c2ee900e7669329337a11d8a63f33b21d17ae91
seror <seror94ab@yahoo.com>
2018-08-20 19:13:20 -0500

    persisting order state

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,9 +5,16 @@ class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === "available";
+    const isAvailable = fish && fish.status === "available";
+    // Make sure the fish is loaded before we continue
+    if (!fish) return null;
+
     if (!isAvailable) {
-      return <li key={key}>Sorry {fish ? fish.name : "fish"} is no longer available</li>;
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
     }
     return (
       <li key={key}>
@@ -16,7 +23,6 @@ class Order extends React.Component {
       </li>
     );
   };
-
 
   render() {
     const orderIds = Object.keys(this.props.order);
