0e7cb100c8b8f04180bc0cd560572c677d02edca
seror <seror94ab@yahoo.com>
2018-08-21 13:23:15 -0500

    rendered edit fish form

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -2,7 +2,17 @@ import React from "react";
 
 class EditFishForm extends React.Component {
   render() {
-    return <p>Edit Fish</p>;
+    return (
+      <div className="fish-edit">
+        <input type="text" name="name" />
+        <input type="text" name="price" />
+        <select type="text" name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <input type="text" name="image" />
+      </div>
+    );
   }
 }
 
